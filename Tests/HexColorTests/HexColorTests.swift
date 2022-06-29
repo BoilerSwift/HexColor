46fada57bb113060fa89047aa795feb5e79a886c
lgvv <lgw9898@naver.com>
2022-06-29 23:53:55 +0900

    [ADD] init

---
+++ Tests/HexColorTests/HexColorTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import HexColor
+
+final class HexColorTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(HexColor().text, "Hello, World!")
+    }
+}
