3aa8a253df19e3beefcbbacea5c619ee080b4657
lgvv <lgw9898@naver.com>
2022-07-01 02:20:19 +0900

    [추가] Hexcode로 Color, UIColor를 사용하는 코드를 추가합니다.

--- Sources/HexColor/HexColor.swift
+++ Sources/HexColor/HexColor.swift
@@ -1,6 +1,34 @@
-public struct HexColor {
-    public private(set) var text = "Hello, World!"
+import UIKit
+import SwiftUI
 
-    public init() {
+extension UIColor {
+    convenience init(hexcode: String) {
+        let scanner = Scanner(string: hexcode)
+        var rgbValue: UInt64 = 0
+        
+        scanner.scanHexInt64(&rgbValue)
+        
+        let red = (rgbValue & 0xff0000) >> 16
+        let green = (rgbValue & 0xff00) >> 8
+        let blue = rgbValue & 0xff
+        
+        self.init(red: Double(red) / 0xff, green: Double(green) / 0xff, blue: Double(blue) / 0xff, alpha: 1.0)
+    }
+}
+
+@available(iOS 13.0, *)
+extension Color {
+    init(hexcode: String) {
+        let scanner = Scanner(string: hexcode)
+        var rgbValue: UInt64 = 0
+        
+        scanner.scanHexInt64(&rgbValue)
+        
+        let red = (rgbValue & 0xff0000) >> 16
+        let green = (rgbValue & 0xff00) >> 8
+        let blue = rgbValue & 0xff
+        
+        self.init(red: Double(red) / 0xff, green: Double(green) / 0xff, blue: Double(blue) / 0xff)
+        
     }
 }
